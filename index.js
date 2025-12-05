f7380e8c9660c8180f966a1f6e4beed6e1987c59
tjgealone079-hue <tjgealone079@gmail.com>
2025-12-05 16:13:49 +0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,28 @@
+const navbar = document.querySelector('.navigation');
+const navItems = document.querySelectorAll('.nav-item');
+const sections = document.querySelectorAll('section, .home, .projects, .technologies, .experience, .about, .contact'); 
+
+window.addEventListener('scroll', () => {
+  if (window.scrollY > 50) {
+    navbar.classList.add('scrolled');
+  } else {
+    navbar.classList.remove('scrolled');
+  }
+
+
+  let currentSection = '';
+  sections.forEach(section => {
+    const sectionTop = section.offsetTop - 70; 
+    const sectionHeight = section.offsetHeight;
+    if (window.scrollY >= sectionTop && window.scrollY < sectionTop + sectionHeight) {
+      currentSection = section.getAttribute('id');
+    }
+  });
+
+  navItems.forEach(item => {
+    item.classList.remove('active');
+    if (item.getAttribute('href') === '#' + currentSection) {
+      item.classList.add('active');
+    }
+  });
+});
